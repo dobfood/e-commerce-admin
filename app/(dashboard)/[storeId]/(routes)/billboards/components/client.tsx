8e8c4412577e98a5e05535457ccd84ee208af5e8
anhkiet <kiet47579@gmail.com>
2023-07-21 20:56:47 +0700

    push

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+interface BillboardClientProps{
+  data:BillboardColumn[]
+}
+
+export const BillboardClient:React.FC<BillboardClientProps> = ({data}) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboard (${data.length})`}
+          description="Manage billboard for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add new
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="label" columns={columns} data={data}/>
+    </>
+  );
+};
